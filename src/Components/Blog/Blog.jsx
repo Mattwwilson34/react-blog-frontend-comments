bbf888792c984c0a474e5bc5c67d49761b80146f
Matt Wilson <mattwwilson34@gmail.com>
2022-05-30 22:26:43 -0400

    added blog and comment data displays

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,23 +1,43 @@
 import './Blog.css';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const Blog = (props) => {
-  const { title, text, url, formatted_date_published, author } = props.blog;
-  console.log(url);
+  // Destructure blog_post
+  const { title, text, published, comments, author } = props.blog;
+
+  // Destructure author
+  const { first_name, last_name, email } = author;
+
   return (
-    <NavLink className='Blog__NavLink' to={url}>
-      <div className='Blog'>
-        <h2 className='Blog__Title'>{title}</h2>
-        <span className='Blog__Author'>
-          Author: {author.first_name} {author.last_name}
-        </span>
-        <p className='Blog__Text'>{text}</p>
-        <div className='Blog__Published'>
-          Published: {formatted_date_published}
+    <>
+      {!props.blog ? (
+        <div>...Blog Loading...</div>
+      ) : (
+        <div>
+          <h1>{title}</h1>
+          <span>Published: {published ? 'true' : 'false'}</span>
+          <div>
+            Author: {first_name} {last_name} ({email})
+          </div>
+          <p>{text}</p>
+          <div>
+            {comments.map((comment) => {
+              console.log(comment);
+              /* TODO Extract Comment into own component with set styles */
+              return (
+                <div key={comment._id}>
+                  <div>{comment.author.username}</div>
+                  <div>{comment.text}</div>
+                  <div>
+                    {comment.date_yyyy_mm_dd} at {comment.time_hour_min}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </NavLink>
+      )}
+    </>
   );
 };
 
