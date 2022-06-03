28230cb93428dda05b108a64fcedbfbd37a99516
Matt Wilson <mattwwilson34@gmail.com>
2022-06-03 15:23:08 -0700

    updated form POST logic

--- src/Components/CommentForm/CommentForm.jsx
+++ src/Components/CommentForm/CommentForm.jsx
@@ -1,26 +1,38 @@
 // Utils
 import React, { useState } from 'react';
+import chalk from 'chalk';
+
 // Style sheets
 import './CommentForm.css';
 
-const CommentForm = () => {
+const CommentForm = (props) => {
   // State
   const [comment, setComment] = useState('');
 
+  // Props
+  const { blogId, authorId } = props;
+
+  // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: 'React Hooks POST Request Example' }),
+      body: JSON.stringify({
+        text: comment,
+        author: authorId,
+        date_published: new Date(),
+        blog_post: blogId,
+      }),
     };
     const response = await fetch(
       'http://localhost:8080/comment',
       requestOptions
     );
     const data = await response.json();
-    console.log(data);
+    console.log(chalk.bgGreen(data));
+    // Reload page
+    window.location.reload();
   };
 
   const handleCommentChange = (e) => setComment(e.target.value);
