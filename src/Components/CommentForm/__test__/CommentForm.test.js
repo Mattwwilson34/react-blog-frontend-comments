c3ad17512c06b8e856ffec80c254a2f76ac5ed74
Matt Wilson <mattwwilson34@gmail.com>
2022-06-03 14:03:11 -0700

    initial commit

---
+++ src/Components/CommentForm/__test__/CommentForm.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CommentForm from '../CommentForm';
+
+describe('CommentForm component', () => {
+  it('Does something...', () => {
+
+  });
+});
