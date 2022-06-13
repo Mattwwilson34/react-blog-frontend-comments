24c45e90787043990aa809242e654748f015ab00
Matt Wilson <mattwwilson34@gmail.com>
2022-06-12 20:23:12 -0400

    Added logout route

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import HomeRoute from './routes/Home';
 import BlogsIndex from './routes/BlogsIndex';
 import Blog from './routes/Blog';
 import Login from './routes/Login';
+import Logout from './routes/Logout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,6 +25,7 @@ root.render(
         <Route path='/blog_posts' element={<BlogsIndex />}></Route>
         <Route path='/blog_posts/:blogId' element={<Blog />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/logout' element={<Logout />}></Route>
       </Route>
     </Routes>
   </BrowserRouter>
